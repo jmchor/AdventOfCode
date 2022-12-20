408f9481f8bd3bd4196948a3f419adfd5d0c6ddb
Johannes Chorzempa <jchorzempa@posteo.org>
2022-12-20 14:13:39 +0100

    part 2

--- day3.js
+++ day3.js
@@ -3,28 +3,34 @@ let sumArray = [];
 const alphabet = '0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
 
-        input.forEach((line) => {
+//         input.forEach((line) => {
 
-            const mid = line.length / 2;
-            const firstHalf = line.slice(0, mid);
-            const secondHalf = line.slice(mid);
-            let lineArray = []
+//             const mid = line.length / 2;
+//             const firstHalf = line.slice(0, mid);
+//             const secondHalf = line.slice(mid);
+//             let lineArray = []
 
-                for (let i = 0; i < firstHalf.length; i++){
+//                 for (let i = 0; i < firstHalf.length; i++){
 
-                    if (secondHalf.includes(firstHalf[i]) && !lineArray.includes(firstHalf[i])) {
+//                     if (secondHalf.includes(firstHalf[i]) && !lineArray.includes(firstHalf[i])) {
+//                         lineArray.push(firstHalf[i])
+//                     }
+//                 }
+//             sumArray.push(lineArray)
+//         })
+
+// let sum = 0;
+//     sumArray = sumArray.flat(1)
+
+//     for (let i = 0; i < sumArray.length; i++) {
+//         sum += alphabet.indexOf(sumArray[i])
+//     }
+
+let lineArray = []
+
+                for (let i = 0; i < input.length; i++){
+
+                    if (input.includes(input[i]) && !lineArray.includes(firstHalf[i])) {
                         lineArray.push(firstHalf[i])
                     }
                 }
-            sumArray.push(lineArray)
-        })
-
-let sum = 0;
-    sumArray = sumArray.flat(1)
-
-    for (let i = 0; i < sumArray.length; i++) {
-        sum += alphabet.indexOf(sumArray[i])
-    }
-
-console.log(sum);
-
