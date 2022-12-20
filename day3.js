ff6fd1551adf49db8efd182ffd27a10e3008b5c3
Johannes Chorzempa <jchorzempa@posteo.org>
2022-12-20 13:56:10 +0100

    Add part 1 solution

--- day3.js
+++ day3.js
@@ -1,7 +1,30 @@
 const input = require('fs').readFileSync('./d3_input.txt', 'utf-8').split('\n');
+let sumArray = [];
+const alphabet = '0abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
 
-const mid = input[i].length / 2;
-const firstHalf = input[i].slice(0, mid);
-const secondHalf = input[i].slice(mid);
 
-console.log('input', input);
+        input.forEach((line) => {
+
+            const mid = line.length / 2;
+            const firstHalf = line.slice(0, mid);
+            const secondHalf = line.slice(mid);
+            let lineArray = []
+
+                for (let i = 0; i < firstHalf.length; i++){
+
+                    if (secondHalf.includes(firstHalf[i]) && !lineArray.includes(firstHalf[i])) {
+                        lineArray.push(firstHalf[i])
+                    }
+                }
+            sumArray.push(lineArray)
+        })
+
+let sum = 0;
+    sumArray = sumArray.flat(1)
+
+    for (let i = 0; i < sumArray.length; i++) {
+        sum += alphabet.indexOf(sumArray[i])
+    }
+
+console.log(sum);
+
