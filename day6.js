63527245841f2c13d61eb4295af88a3c206d5ea0
Johannes Chorzempa <jchorzempa@posteo.org>
2022-12-27 20:30:57 +0100

    Add pseudocode

---
+++ day6.js
@@ -0,0 +1,10 @@
+const input = require('fs').readFileSync('./d6_input.txt', 'utf-8').split('');
+
+
+
+//push every letter of input array into a new array
+//check if the last four letters in the new array are unique by creating a temporary array with the last four letters and check it against a new Set
+
+
+
+console.log(input)
