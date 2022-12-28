3ee33e7bdc027a4faff479b3f31ec467e1c33094
Johannes Chorzempa <jchorzempa@posteo.org>
2022-12-28 20:28:30 +0100

    Add possible solution

--- day8.js
+++ day8.js
@@ -1,23 +1,45 @@
 const input = require('fs').readFileSync('./d8_input.txt', 'utf-8').split('\n');
 const forrestMap = input.map(line => line.split(''));
 
+//include a rotated version of the map where columns are rows
+const rotatedMap = forrestMap[0].map((_, i) => forrestMap.map(row => row[i]));
+
+
 //Part 1
 
 let sum = 0;
+let tallestFromTop = forrestMap[0][1];
 
 //columns
-for (let i = 1; i < forrestMap.length; i++) {
+for (let i = 1; i < forrestMap.length -1; i++) {
 
-    //rows
-    for (let j = 1; j < forrestMap[0].length; j++) {
+    let tallestFromLeft = forrestMap[i][0];
+    let tallestFromRight = forrestMap[i][forrestMap[0].length -1];
+    //rows from left
+    for (let j = 1; j < forrestMap[0].length -1; j++) {
 
-        //determine if j is bigger than every item before it OR if js is bigger than every item after it
+        //check if the current tree is taller than the tallest so far from the left OR
+        //the tallest so far from the top by comparing the same sections but in the rotated map
 
-        if(
-
+        if (tallestFromLeft < forrestMap[i][j] || tallestFromTop < forrestMap[i][j]) {
+            tallestFromLeft = forrestMap[i][j];
+            sum++;
+        }
 
     }
+    //rows from right
+    for (let j = forrestMap[0].length -2; j >= 1; j--) {
 
+        console.log(`In row ${i}: The tallest so far is ${tallestFromRight}`)
+        if (tallestFromRight < forrestMap[i][j]) {
 
+            tallestFromRight = forrestMap[i][j];
+            console.log(`In row ${i}: The new tallest so far is ${tallestFromRight}`)
+            sum++;
+            console.log(sum)
+        }
+    }
 
 }
+
+console.log(forrestMap)
