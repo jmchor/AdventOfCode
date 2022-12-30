2ac5372ff30b1ec05dac52c24c3334fffb5cb2bc
Johannes Chorzempa <jchorzempa@posteo.org>
2022-12-30 10:31:02 +0100

    Found solution

--- day8.js
+++ day8.js
@@ -1,4 +1,3 @@
-console.time()
 const input = require('fs').readFileSync('./d8_input.txt', 'utf-8').split('\n');
 const forrestMap = input.map(line => line.split(''));
 
@@ -9,128 +8,104 @@ const emptyMap = forrestMap.map(row => row.map(_ => 0));
 let sum = 0
 //Part 1
 
-// // row left to right
-// for (let i = 1; i < forrestMap.length -1; i++) {
+// row left to right
+//     for (let i = 1; i < forrestMap.length -1; i++) {
 
-//     let tallestFromLeft = forrestMap[i][0];
-//     let tallestFromTop = forrestMap[0][i];
+//         let tallestFromLeft = forrestMap[i][0];
+//         let tallestFromTop = forrestMap[0][i];
 
-//     for (let j = 1; j < forrestMap[0].length -1; j++) {
+//         for (let j = 1; j < forrestMap[0].length -1; j++) {
 
-//         if (tallestFromLeft < forrestMap[i][j] ) {
-//             emptyMap[i][j] = 1;
-//             tallestFromLeft = forrestMap[i][j];
+//             if (tallestFromLeft < forrestMap[i][j] ) {
+//                 emptyMap[i][j] = 1;
+//                 tallestFromLeft = forrestMap[i][j];
+//             }
+//             if (tallestFromTop < forrestMap[j][i] ) {
+//                 emptyMap[j][i] = 1;
+//                 tallestFromTop = forrestMap[j][i];
+//             }
+//         }
+
+//         let tallestFromRight = forrestMap[i][forrestMap[0].length -1];
+//         let tallestFromBottom = forrestMap[forrestMap.length -1][i];
+
+//         for (let j = forrestMap[0].length -2; j >= 1; j--) {
+
+//             if (tallestFromRight < forrestMap[i][j] ) {
+//                 emptyMap[i][j] = 1;
+//                 tallestFromRight = forrestMap[i][j];
+//             }
+//             if (tallestFromBottom < forrestMap[j][i] ) {
+//                 emptyMap[j][i] = 1;
+//                 tallestFromBottom = forrestMap[j][i];
+//             }
+//         }
 //     }
-//         if (tallestFromTop < forrestMap[j][i] ) {
-//             emptyMap[j][i] = 1;
-//             tallestFromTop = forrestMap[j][i];
-//     }
-// } }
 
 
-
-// for (let i = 1; i < forrestMap.length -1; i++) {
-
-//     let tallestFromRight = forrestMap[i][forrestMap[0].length -1];
-//     let tallestFromBottom = forrestMap[forrestMap.length -1][i];
-
-//     for (let j = forrestMap[0].length -2; j >= 1; j--) {
-
-//         if (tallestFromRight < forrestMap[i][j] ) {
-//             emptyMap[i][j] = 1;
-//             tallestFromRight = forrestMap[i][j];
-//     }
-//         if (tallestFromBottom < forrestMap[j][i] ) {
-//             emptyMap[j][i] = 1;
-//             tallestFromBottom = forrestMap[j][i];
-// }
-// } }
-
-
-
-// emptyMap.forEach(row => {
-
-//     sum += row.reduce((acc, curr) => acc + curr, 0)
-
-// })
-
+// emptyMap.forEach(row => { sum += row.reduce((acc, curr) => acc + curr, 0)})
 // let ultimateSum = sum + forrestMap.length * 4 - 4
-
 
 
 // console.log(ultimateSum)
 
-// console.timeEnd()
-
-
-
-
 
 //Part 2
 
-//for every tree, count the number of trees between it and a tree that is higher than it
-//count in every direction
+let absoluteHighScore = 0
+
+    for (let i = 1; i < forrestMap.length-1; i++) {
+
+        for (let j = 1; j < forrestMap.length-1; j++) {
+
+            let currentTree = forrestMap[i][j];
+            //shorter version is to just set total and have total be total *= count after every inner loop; this is more verbose
+            let left = 0
+            let right = 0
+            let top = 0
+            let bottom = 0
+            let count = 0
 
 
-
-let maxViewToRight = 0;
-let viewToRight = 0;
-
-
-for (let i = 0; i < forrestMap.length; i++) {
-
-    for (let j = 0; j < forrestMap.length; j++) {
-
-        let currentTree = forrestMap[i][j];
-        viewToRight = 0;
-        //to right
-        for (let k = 0; k < forrestMap.length; k++) {
-
-            if (currentTree > forrestMap[i][j + k + 1]) {
-                viewToRight += 1;
-            } else if (currentTree <= forrestMap[i][j + k + 1]) {
-                viewToRight += 1;
-
-                break;
+            //To the right
+            for (let x = j+1; x < forrestMap.length; x++) {
+                count++
+                if (currentTree <= forrestMap[i][x]) break;
             }
+            right = count
 
-            if (viewToRight > maxViewToRight) {
-                maxViewToRight = viewToRight;
+            //To the left
+            count = 0 //reset count
+            for (let x = j-1; x >= 0; x--) {
+                count++
+                if (currentTree <= forrestMap[i][x]) break;
+            }
+            left = count
 
+            //To the top
+            count = 0
+            for (let y= i-1; y >= 0; y--) {
+                count++
+                if (currentTree <= forrestMap[y][j]) break;
+            }
+            top = count
+
+            //To the bottom
+            count = 0
+            for (let y= i+1; y < forrestMap.length; y++) {
+                count++
+                if (currentTree <= forrestMap[y][j]) break;
+            }
+            bottom = count
+
+            let total = left * right * top * bottom
+
+            if (total > absoluteHighScore) {
+                absoluteHighScore = total
             }
         }
-
     }
-}
-console.log(forrestMap)
-console.log(maxViewToRight)
 
 
+    console.log(absoluteHighScore)
 
-
-
-
-
-
-
-
-
-
-
-
-
-// let maxViewLeft = 0;
-// for (let i = 0; i < forrestMap.length; i++) {
-
-//     for (let j = 0; j < forrestMap.length; j++) {
-
-//        let currentTree = forrestMap[i][j];
-
-//        if (currentTree > forrestMap[i][j+1]) {
-//            maxViewLeft += 1;
-//        } else if (currentTree < forrestMap[i][j+1]) {
-//            break;
-//        }
-//     }
-
-// }
